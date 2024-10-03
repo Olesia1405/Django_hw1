d6882587e7a3bf4e16af02cca1a60c381eb23ded
Olesia Arkhishina <143208496+Olesia1405@users.noreply.github.com>
2024-10-03 19:15:57 +0300

    Initial commit

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import os
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
@@ -8,8 +10,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,13 +25,19 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    current_time = f"Текущее время: {datetime.now().time()} <br><a href='{reverse('homepage')}'>На главную</a>"
+    return HttpResponse(current_time)
 
 
 def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    path = "/home/ideapad5/Нетология. Python-разработчик с нуля. PD-58/Django/1. Знакомство с Django. Подготовка и запуск проекта./Django_Homework1_Introduction"
+    filelist = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            filelist.append(os.path.join(root, file))
+    all_dir_files = f"Содержимое рабочей директории: {filelist} <br><a href='{reverse('homepage')}'>На главную</a>"
+
+    return HttpResponse(all_dir_files)
