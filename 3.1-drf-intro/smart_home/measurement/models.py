a40a1fd0e92a00db38454226352db89a31e0ec67
Olesia Arkhishina <143208496+Olesia1405@users.noreply.github.com>
2024-11-03 14:03:12 +0300

    Initial commit

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50, blank=True)
+
+class Measurement(models.Model):
+    temperature = models.DecimalField(max_digits=3, decimal_places=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    measurements = models.ForeignKey(Sensor, related_name='data_m', on_delete=models.CASCADE, blank=False, null=False,)
--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,27 @@
 from rest_framework import serializers
+from measurement.models import Measurement, Sensor
 
-# TODO: опишите необходимые сериализаторы
+
+class SensorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = '__all__'
+
+
+class MeasurementDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['temperature', 'created_at']
+
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    data_m = MeasurementDetailSerializer(read_only=True, many=True)
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'data_m']
